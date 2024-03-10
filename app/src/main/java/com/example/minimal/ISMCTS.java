53a8c6c400f87363e4ed1021408b72ab130c470b
Maleeha13 <maleehamatto7@gmail.com>
2024-03-10 17:29:32 +0000

    javadocs for ISMCTS

--- app/src/main/java/com/example/minimal/ISMCTS.java
+++ app/src/main/java/com/example/minimal/ISMCTS.java
@@ -1,6 +1,5 @@
 package com.example.minimal;
 
-
 import android.os.Handler;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -9,29 +8,57 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Random;
 
 import android.os.AsyncTask;
-import android.os.Handler;
 import android.os.Looper;
 
+/**
+ * This class is responsible for executing the main loop of the ISMCTS
+ */
 public class ISMCTS {
 
+    /**
+     * Executes a Monte Carlo simulation task in the background.
+     *
+     * @param rootState   The initial state of the game.
+     * @param itermax     The maximum number of iterations to run in the Monte Carlo simulation.
+     * @param hand        The list of ImageViews representing the player's hand.
+     * @param dropButton  The button used for dropping cards from the hand.
+     * @param game        The instance of the Game class managing the game state.
+     * @param deck        The ImageView representing the deck of cards.
+     * @param stack       The ImageView representing the stack or discard pile of cards.
+     */
     public void runInBackground(State rootState, int itermax, List<ImageView> hand, Button dropButton, Game game, ImageView deck, ImageView stack) {
         new MonteCarloTask(rootState, itermax, hand, dropButton, game, deck, stack).execute();
     }
 
+    /**
+     * AsyncTask subclass for running a Monte Carlo simulation task in the background.
+     * This task is responsible for evaluating possible moves in a game using Monte Carlo simulation.
+     */
     private class MonteCarloTask extends AsyncTask<Void, Void, Move> {
-        private State rootState;
-        private int itermax;
-        private List<ImageView> hand;
-        private Button dropButton;
-        private Game game;
-        private ImageView deck;
-        private ImageView stack;
+        private State rootState;    // Initial state of the game
+        private int itermax;        // Max number of iterations to run the loop
+        private List<ImageView> hand;   // The list of ImageViews representing the player's hand.
+        private Button dropButton;          // The button used for dropping cards from the hand.
+        private Game game;                  // The instance of the Game class managing the game state.
+        private ImageView deck;             // The ImageView representing the deck of cards.
+        private ImageView stack;            // The ImageView representing the stack or discard pile of cards.
 
+
+        /**
+         * Constructs a new MonteCarloTask with the specified parameters.
+         *
+         * @param rootState   The initial state of the game.
+         * @param itermax     The maximum number of iterations to run in the Monte Carlo simulation.
+         * @param hand        The list of ImageViews representing the player's hand.
+         * @param dropButton  The button used for dropping cards from the hand.
+         * @param game        The instance of the Game class managing the game state.
+         * @param deck        The ImageView representing the deck of cards.
+         * @param stack       The ImageView representing the stack or discard pile of cards.
+         */
         public MonteCarloTask(State rootState, int itermax, List<ImageView> hand, Button dropButton, Game game, ImageView deck, ImageView stack) {
             this.rootState = rootState;
             this.itermax = itermax;
@@ -42,54 +69,62 @@ public class ISMCTS {
             this.stack = stack;
         }
 
+
+        /**
+         * Runs the Monte Carlo Tree Search (MCTS) algorithm in the background to find the best move.
+         *
+         * @param voids Not used.
+         * @return The best move found by the ISMCTS algorithm.
+         */
         @Override
         protected Move doInBackground(Void... voids) {
+            // Create the root node of the Monte Carlo Tree
             MCTSNode rootNode = new MCTSNode(rootState, null, 0, null);
 
+            // Expand the root node
             expandNode(rootNode, rootState);
 
+            // Run Monte Carlo simulations itermax times
             for (int i = 0; i < itermax; i++) {
+                // Clone the root state for simulation
                 State state = rootState.clone();
                 try {
-                    List<Integer> card = state.updateUnseenCards();
-//                    System.out.println("is it terminal " + state.isTerminal() + " size is "  + card.size());
+                    // Update unseen cards and randomize the state
+                    state.updateUnseenCards();
                     state = state.CloneAndRandomize(rootState.getPlayerToMove());
                 } catch (CloneNotSupportedException e) {
 
                     throw new RuntimeException(e);
                 }
+
+                // Clone the root node for simulation
                 MCTSNode node = rootNode.cloneNode();
 
+                // Selection phase: descend the tree until a leaf node is reached
                 while (!state.getAllMoves(state.getPlayerToMove()).isEmpty() && node.getUntriedMoves(state).isEmpty() && !state.isTerminal()) {
                     node = node.UCBSelectChild(state.getAllMoves(state.getPlayerToMove()), 0.8);
-
-//                    System.out.println("moves 1 " + state.updateUnseenCards().size());
                     state.applyMove(node.getMove(), state.getPlayerToMove());
-//
                 }
 
+                // Expansion phase: expand the leaf node if possible
                 List<Move> untriedMoves = node.getUntriedMoves(state);
-                if (!untriedMoves.isEmpty() && !state.isTerminal()) {  // if we can expand (i.e. state/node is non-terminal)
+                if (!untriedMoves.isEmpty() && !state.isTerminal()) {
                     Move m = untriedMoves.get(new Random().nextInt(untriedMoves.size()));
                     int player = state.getPlayerToMove();
-//                    System.out.println("moves 2 " + state.updateUnseenCards().size());
-
                     state.applyMove(m, player);
-                    node = node.addChild(m, player);  // add child and descend tree
+                    node = node.addChild(m, player);
                 }
 
+                // Simulation phase: simulate random moves until the end of the game
                 while (!state.getAllMoves(state.getPlayerToMove()).isEmpty() && !state.isTerminal()) {
-
                     List<Move> legalMoves = state.getAllMoves(state.getPlayerToMove());
                     Move randomMove = legalMoves.get(new Random().nextInt(legalMoves.size()));
-//                    System.out.println("moves 3 " + state.updateUnseenCards().size());
-
                     state.applyMove(randomMove, state.getPlayerToMove());
                     boolean simulationResult = state.getResult(state.getPlayerToMove());
                     node.update(state);
-
                 }
 
+                // Backpropagation phase: update the statistics of nodes in the tree
                 while (node != null) {
                     node.update(state);
                     node = node.getParent();
@@ -97,17 +132,20 @@ public class ISMCTS {
                 }
             }
 
+            // Choose the best move based on the statistics of child nodes
             if (!rootNode.getChildren().isEmpty()) {
-                System.out.println("Move by ISMCTS: " + Collections.max(rootNode.getChildren(), Comparator.comparingInt(MCTSNode::getWins)).getMove());
-//                return Collections.min(rootNode.getChildren(), Comparator.comparingDouble(MCTSNode::getScore)).getMove();
-
                 return Collections.max(rootNode.getChildren(), Comparator.comparingInt(MCTSNode::getWins)).getMove();
             } else {
-                System.out.println("move is " + rootNode.getMove());
                 return rootNode.getMove();
             }
         }
 
+        /**
+         * Executes after the background computation finishes.
+         * Applies the best move found by the ISMCTS algorithm to the game UI.
+         *
+         * @param maxMove The best move found by the ISMCTS algorithm.
+         */
         @Override
         protected void onPostExecute(Move maxMove) {
             if (maxMove != null) {
@@ -119,21 +157,34 @@ public class ISMCTS {
         }
     }
 
+    /**
+     * Expands the given node by adding a child node corresponding to a randomly chosen untried move in the given state.
+     * If there are no untried moves available, the node remains unchanged.
+     *
+     * @param node  The node to expand.
+     * @param state The state of the game associated with the node.
+     */
     private void expandNode(MCTSNode node, State state) {
         List<Move> untriedMoves = new ArrayList<>(node.getUntriedMoves(state));
-//        System.out.println("untried moves " + untriedMoves.isEmpty() + " terminal " + state.isTerminal());
         if (!untriedMoves.isEmpty()) {
             Move randomMove = untriedMoves.get(new Random().nextInt(untriedMoves.size()));
             State newState = state.clone();
-//            System.out.println("moves 4 " + state.updateUnseenCards().size());
-
             newState.applyMove(randomMove, newState.getPlayerToMove());
             node.addChild(randomMove, newState.getPlayerToMove());
-//            System.out.println("added chikd to root");
             untriedMoves.remove(randomMove);
         }
     }
 
+    /**
+     * Applies the best move determined by the Monte Carlo simulation to the game UI.
+     *
+     * @param maxMove    The best move determined by the Monte Carlo simulation.
+     * @param hand       The list of ImageViews representing the player's hand.
+     * @param dropButton The button used for dropping cards from the hand.
+     * @param game       The instance of the Game class managing the game state.
+     * @param deck       The ImageView representing the deck of cards.
+     * @param stack      The ImageView representing the stack or discard pile of cards.
+     */
     public void applyBest(Move maxMove, List<ImageView> hand, Button dropButton, Game game, ImageView deck, ImageView stack) {
         new Handler(Looper.getMainLooper()).post(new Runnable() {
             @Override
@@ -141,15 +192,15 @@ public class ISMCTS {
                 String source = maxMove.getSource();
                 List<Integer> cards = maxMove.getCardsPlayed();
 
+                // Simulate clicking on the ImageViews corresponding to the cards played in the best move
                 for (ImageView imageView : hand) {
                     int tagValue = (Integer) imageView.getTag();
                     if (cards.contains(tagValue)) {
                         imageView.performClick();
-
-
                     }
                 }
 
+                // Simulate clicking on the dropButton after a delay
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
@@ -157,6 +208,7 @@ public class ISMCTS {
                     }
                 }, 500);
 
+                // Simulate clicking on the deck or stack based on the source of the best move
                 if (Objects.equals(source, "deck")) {
                     new Handler().postDelayed(new Runnable() {
                         @Override
@@ -178,36 +230,29 @@ public class ISMCTS {
         });
     }
 
+
+    /**
+     * Checks if the sum of values of cards in the player's hand is less than 6.
+     * If so, simulates a click on the "show" button.
+     * Additionally, calculates the sum of values of cards in each player's hand and stores them in an array.
+     *
+     * @param myCards The list of ImageViews representing the player's hand.
+     * @param game    The instance of the Game class managing the game state.
+     * @param show    The button used to indicate readiness to reveal cards.
+     */
     public void show( List<ImageView> myCards, Game game, Button show) {
         int val = 0;
+
+        // Calculate the sum of values of cards in the player's hand
         for (ImageView imageView : myCards) {
             int tag = (Integer) imageView.getTag();
             val += tag % 100;
         }
 
+        // If the sum is less than 6, simulate a click on the "show" button
         if (val < 6) {
             show.performClick();
         }
-        int[] handSums = new int[4]; // Create an array to store hand sums
-
-        int index = 0;
-
-        for (Map.Entry<Integer, List<Integer>> entry : game.playerHand.entrySet()) {
-            List<Integer> hand = entry.getValue();
-            // Calculate sum of the player's hand
-            int sum = 0;
-            for (Integer card : hand) {
-                sum += card % 100;
-            }
-
-            // Store the sum in the array
-            handSums[index] = sum;
-            index++;
-
-        }
-
-        for (int sum : handSums) {
-//            System.out.println(sum);
-        }
     }
+
 }
