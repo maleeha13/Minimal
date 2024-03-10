4ec2154a44c6bdca690621c1a4ca6e07e04c688a
Maleeha13 <maleehamatto7@gmail.com>
2024-03-10 16:46:31 +0000

    javadocs for file writer

--- app/src/main/java/com/example/minimal/fileWriter.java
+++ app/src/main/java/com/example/minimal/fileWriter.java
@@ -3,6 +3,10 @@ package com.example.minimal;
 import android.content.Context;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
+/**
+ * File writer class is used to log details into a text file for testing and eval purposes
+ */
 public class fileWriter {
     public static void appendToFile(Context context, String fileName, String data) {
         FileOutputStream fos = null;
